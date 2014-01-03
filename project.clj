03de581b62640330cf5955d642172fda18ac666a
Matt Crinklaw-Vogt <matt.crinklaw@gmail.com>
2014-01-02 21:41:53 -0500

    Generates json struct from javadoc objects

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :resource-paths [~(str (System/getenv "JAVA_HOME") "/lib/tools.jar")] ;
   :aot [com.tantaman.doc.JsonDoclet]
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/data.json "0.2.3"]])
--- src/com/tantaman/doc/JsonDoclet.clj
+++ src/com/tantaman/doc/JsonDoclet.clj
@@ -1,6 +1,33 @@
 (ns com.tantaman.doc.JsonDoclet
+  (:require [clojure.data.json :as json])
   (:gen-class 
-    :methods [#^{:static true} [start [com.sun.javadoc.RootDoc] void]]))
+    :methods [#^{:static true} [start [com.sun.javadoc.RootDoc] boolean]]))
 
+(def dir "./docs")
 
-(defn start [root])
+(defn write-class [docmap]
+  (spit (str dir "/" (docmap "qualifiedName")) (json/write-str docmap)))
+
+(declare extract-docs)
+(def doc-keys 
+  {
+   "name" #(.name %)
+   "annotations" #(map extract-docs (.annotations %))
+   "qualifiedName" #(.qualifiedName %)
+   "methods" #(map extract-docs (.methods %))
+   "docString" #(.commentText %)
+   "fields" #(map extract-docs (.fields %))
+   "interfaces" #(map extract-docs (.interfaces %))
+  })
+
+(defn extract-docs [doc-obj]
+  (reduce-kv
+    (fn [docmap k extractor] 
+      (try 
+        (assoc docmap k (extractor doc-obj))
+        (catch Exception e docmap)))
+   {} doc-keys))
+
+(defn -start [root]
+  (pmap write-class (map extract-docs (.classes root)))
+  true)
